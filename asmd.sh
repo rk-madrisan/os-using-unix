3557f43d96b03c8442c32d81125f3109ebfafcb6
rk-madrisan <rk.riyas@outlook.com>
2024-12-11 00:09:55 +0530

    Create asmd.sh

---
+++ asmd.sh
@@ -0,0 +1,17 @@
+echo "Enter the value a : "
+read a
+echo "Enter the value b : "
+read b
+
+
+sum=$((a + b))
+echo " Sum of two values : "$sum
+
+sub1=$((a - b))
+echo " Subraction of two values : "$sub1
+
+multi=$((a * b))
+echo " Multiplication of two values : "$multi
+
+divi=$((a / b))
+echo " Division of two values : "$divi
